ec6daa68e007586f6844dd4a695565973ef66367
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-05-09 16:11:36 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -16,5 +16,9 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     return s;
 }
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats) {
+    if(computedStats.average > maxThreshold) {
+        emailAlertCallCount = 1;
+        ledAlertCallCount = 1;
+    }
+}
