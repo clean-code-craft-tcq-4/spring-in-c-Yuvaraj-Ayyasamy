ba7616d195d4f13d18410399c703e26651acd56d
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-05-10 12:07:05 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -30,7 +30,7 @@ void ledAlerter() {
     ledAlertCallCount++; 
 }
 
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], const struct Stats computedStats) {
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats) {
     if(computedStats.max > maxThreshold) {
         (alerters[0])();
         (alerters[1])();
