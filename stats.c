a8c13ec3e18f8c33bfc1892fda366318f80cf094
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-05-10 12:00:18 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -5,7 +5,31 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    if((numberset != 0) && (setlength != 0)) {
+        s.min = numberset[0];
+        for(int i=0; i<setlength; i++) {
+            if(s.min > numberset[i])
+                s.min = numberset[i];
+            if(s.max < numberset[i])
+                s.max = numberset[i];
+            s.average += numberset[i];
+        }
+        s.average /= setlength;
+    }
+    return s;
 }
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
+void emailAlerter() {
+    emailAlertCallCount++;
+}
+
+void ledAlerter() {
+    ledAlertCallCount++; 
+}
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], const struct Stats computedStats) {
+    if(computedStats.max > maxThreshold) {
+        (alerters[0])();
+        (alerters[1])();
+    }
+}
